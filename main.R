8aae06a8f8e201b5f19c78bc33249bce6a755d67
astridbos <astrid.bos@wur.nl>
2015-01-09 16:29:30 +0100

    data & init

---
+++ main.R
@@ -0,0 +1,12 @@
+# AD-Scripting, Astrid Bos & David Scholte-Albers
+# 09 January 2014
+# Exercise 5
+
+# Start with empty environment
+rm(list=ls())
+
+# Import packages
+library(raster)
+library(rgdal)
+
+# Check working directory
